afe4ae5ca032017701135e6a6e034e1914a344f2
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-07-10 12:58:56 +0530

    Update youtube_data.py

--- youtube_data.py
+++ youtube_data.py
@@ -1,5 +1,4 @@
 #------------------------------------------------------------Import packages and libraries--------------------------------------------------------#
-pip freeze > requirements.txt
 pip install -r requirements.txt
 pip install --upgrade -r requirements.txt
 import pandas as pd
